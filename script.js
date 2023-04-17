a1382ba6b5fed74b9e83d78734d7fa589f80fce3
Noan <delatouchenoan@gmail.com>
2023-04-17 12:47:08 +0200

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// Typed.js code section
+
+var presentationText = new Typed("#changing_desc", {
+    strings: ["Noan Delatouche","a front-end <code><</code>developer<code>></code>","also a back-end $developer"],
+    typeSpeed :50,
+    backSpeed:50,
+    loop:true,
+    backDelay:800,
+});
